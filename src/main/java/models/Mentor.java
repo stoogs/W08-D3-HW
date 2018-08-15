bdfeef7dd7be6e36e38168897922aa32dfc97568
Stuart Black <stuartblack@gmail.com>
2018-08-15 15:38:53 +0100

    Initial setup of files. Mentor class created

---
+++ src/main/java/models/Mentor.java
@@ -0,0 +1,35 @@
+package models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "mentor")
+public class Mentor {
+    private int id;
+    private String name;
+
+    public Mentor(){
+    }
+
+    public Mentor(String name) {
+        this.name = name;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    @Column(name="name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
